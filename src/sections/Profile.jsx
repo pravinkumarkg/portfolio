e91d6a1796972b050423006060ae9830f2281045
Pravin Kumar <pravinkumar.g@elitoinnovations.com>
2026-01-22 18:41:18 +0530

    url added for linkedin and github

--- src/sections/Profile.jsx
+++ src/sections/Profile.jsx
@@ -13,10 +13,10 @@ const Profile = ({refProp}) => {
                         <p className="text-[#717182] mb-4 md:mb-6">Frontend Developer with 6 years of overall experience in developing responsive web layouts using HTML, CSS, and modern JavaScript (ES6+). Possess 1 year of hands-on experience with React.js, focusing on reusable component development and REST API integration. Proficient in UI styling frameworks including Bootstrap, Sass, and Tailwind CSS, with a strong focus on performance and maintainable UI code.</p>
                         <div className="flex gap-3 items-center">
                             <button className="flex gap-2 items-center bg-black px-3 py-2 text-white rounded-lg border cursor-pointer hover:bg-white hover:text-black hover:border-[#000000] transition-all duration-500 ease-in-out"><AiOutlineDownload className="text-[24px]" />View Resume</button>
-                            <a href="">
+                            <a href="https://www.linkedin.com/in/pravin-kumar-k-g-7b9448159/" target="_blank">
                                 <AiFillLinkedin className="text-[24px]" />
                             </a>
-                            <a href="">
+                            <a href="https://github.com/pravinkumarkg" target="_blank">
                                 <AiFillGithub className="text-[24px]" />
                             </a>
                         </div>
