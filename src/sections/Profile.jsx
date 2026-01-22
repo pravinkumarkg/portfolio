4e6cfcb285a6464ae04591ddfd79892eadf4bc07
Pravin Kumar <pravinkumar.g@elitoinnovations.com>
2026-01-22 18:54:56 +0530

    added resume and its redirection

--- src/sections/Profile.jsx
+++ src/sections/Profile.jsx
@@ -12,7 +12,7 @@ const Profile = ({refProp}) => {
                         <p className="sm:text-[16px] md:text-[20px] text-[#717182] mb-4 md:mb-6">6 Years of Experience</p>
                         <p className="text-[#717182] mb-4 md:mb-6">Frontend Developer with 6 years of overall experience in developing responsive web layouts using HTML, CSS, and modern JavaScript (ES6+). Possess 1 year of hands-on experience with React.js, focusing on reusable component development and REST API integration. Proficient in UI styling frameworks including Bootstrap, Sass, and Tailwind CSS, with a strong focus on performance and maintainable UI code.</p>
                         <div className="flex gap-3 items-center">
-                            <button className="flex gap-2 items-center bg-black px-3 py-2 text-white rounded-lg border cursor-pointer hover:bg-white hover:text-black hover:border-[#000000] transition-all duration-500 ease-in-out"><AiOutlineDownload className="text-[24px]" />View Resume</button>
+                            <button onClick={() => window.open("/Pravin-Kumar-K-G-Resume.pdf", "_blank")} className="flex gap-2 items-center bg-black px-3 py-2 text-white rounded-lg border cursor-pointer hover:bg-white hover:text-black hover:border-[#000000] transition-all duration-500 ease-in-out"><AiOutlineDownload className="text-[24px]" />View Resume</button>
                             <a href="https://www.linkedin.com/in/pravin-kumar-k-g-7b9448159/" target="_blank">
                                 <AiFillLinkedin className="text-[24px]" />
                             </a>
